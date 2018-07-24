fb46936c45d050cc475b5d87f981d87f2824b039
Simon Escure <simon_escure@yahoo.fr>
2018-07-24 16:03:52 +0200

    Résolution erreurs JSON

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+
+use App\Controller\DefaultController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 
 class DefaultController extends Controller
 {
-		/**
-     * @route("/", name="homePage"")
-     */
+    /** @Route("/", name="homePage") **/
     public function homeAction(Request $request)
     {
         return $this->render('DefaultPages/home.html.twig');
     }
 }
-
-?>
