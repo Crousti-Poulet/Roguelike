2425b85e37f0548d783365fef151e0d15424ae4c
Crousti-Poulet <el-ouni-mehdi@hotmail.fr>
2018-07-24 17:16:42 +0200

    Un commit pour rien

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,11 +2,10 @@
 
 namespace App\Controller;
 
-
 use App\Controller\DefaultController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 class DefaultController extends Controller
